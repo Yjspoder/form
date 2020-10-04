30e8c17c29c4cfd33890353c966758f41563fce7
Yjspoder <injose.joshi@gmail.com>
2020-10-04 19:32:34 +0530

    Add header

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <nav class="navbar navbar-light bg-light justify-content-center">
+      <span class="navbar-brand mb-0 h1">Multi-Step Form</span>
+    </nav>
+  );
+}
